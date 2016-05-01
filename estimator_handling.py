0dddbfab0f117b65ee12a7a899c2f046d2a681db
Olga Medennikova <penguin.tut@gmail.com>
2016-05-02 01:43:52 +0300

    update functions to use numpy arrays as input instead of pandas DF

--- estimator_handling.py
+++ estimator_handling.py
@@ -3,6 +3,16 @@ from __future__ import division
 import numpy as np
 import pandas as pd
 
+%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+
+# set seaborn plot defaults
+import seaborn as sns;
+sns.set(palette="husl")
+sns.set_context("notebook")
+sns.set_style("whitegrid")
+
 def nparray_to_df(data, store):
     """Transform dataset in numpy array format to a df.
     Return the df.
@@ -23,83 +33,96 @@ def nparray_to_df(data, store):
 
     return df
 
-def class_prob(model, test_data):
-    """Calculate probability for
-    every class and return an array
-    with highest probability for each
-    track.
+def class_size(model_pred):
+    """Count number of items
+    in every class.
     """
-    probability = model.predict_proba(test_data)
+    unique, counts = np.unique(model_pred[:, 0], return_counts=True)
+    return dict(zip(unique, counts))
 
-    # make a list of highest probabilities
-    max_proba = np.asarray([x.max() for x in probability])
-    return max_proba[:, None]
+def class_mean(model_pred, lb_ind):
+    """Return mean probability
+    for the class
+    """
+    return np.mean(model_pred[model_pred[:, 0] == lb_ind][:, 1])
 
-def model_summary(df, model, model_name, labels, cvs):
+def model_summary(model_pred, model,
+                  model_name, labels, cvs):
+    """Print model summary
+    """
     print ("Summary of the {} classifier "
-           "performance\n".format(model_name))
-    print "Model: "
+           "performance \n\nModel: "
+           .format(model_name))
     print model
     print ("\nModel accuracy score: {:.3f}"
-           .format(cvs))
-    # drop 'match' column as it's not relevant here
-    df = df.drop('match', 1)
-    print ("\nNumber of tracks in the test set: {}."
-           .format(len(df)))
-    for lb in labels:
-        print ("Number of tracks assigned to the \"{}\" class: {}, or {:.2%} of all tracks."
-               .format(lb, len(df[df['label'] == labels.index(lb)]),
-                      (len(df[df['label'] == labels.index(lb)])/len(df))))
-    print "\nMean probability for each category"
-    grouped = df.groupby(['label'])
-    print grouped['prob'].mean()
+           "\nNumber of tracks in the test set: {}. \n"
+           .format(cvs, len(model_pred)))
 
-def prob_with_threshold(df, threshold):
-    """Returns a df with class probability
+    for lb in labels:
+        # label index
+        lb_ind = labels.index(lb)
+        # size of the class
+        lb_size = class_size(model_pred)[lb_ind]
+        print ("Number of tracks assigned the \"{}\" class: "
+               "{}, or {:.1%} of all tracks."
+               .format(lb, lb_size,
+                       lb_size / len(model_pred)))
+        print ("Mean probability for the class: {:.3f}. \n"
+               .format(class_mean(model_pred, lb_ind)))
+
+def prob_with_threshold(model_pred, threshold):
+    """Return an array with class probability
     higher than a threshold.
     """
-    # drop 'match' column as it's not relevant here
-    df = df.drop('match', 1)
-
-    prob_thres = df[df['prob'] >= threshold]
+    prob_thres = model_pred[model_pred[:, 1] >= threshold]
     return prob_thres
 
-def sum_category(df, index):
-    """Returns number of tracks in a category
-    """
-    category_size = len(df[df['label'] == index])
-    return category_size
-
-def summary_of_prob_with_thres(df, threshold, labels):
-    df_prob = prob_with_threshold(df, threshold)
-    print ("Total number of tracks to classify: {}."
-           .format(len(df)))
-    print ("Number of tracks with {} probability "
-          "of class membership: {}."
-          .format(threshold, len(df_prob)))
-    print "Among which..."
-    for lb in labels:
-        print ("...assigned the \"{}\" class: {} ({} without the threshold)."
-               .format(lb, len(df_prob[df_prob['label'] == labels.index(lb)]),
-                      len(df[df['label'] == labels.index(lb)])))
-    print "\nMean probability for each class"
-    grouped = df_prob.groupby(['label'])
-    print grouped['prob'].mean()
-
-def plot_prob_thres(df, labels):
+def plot_prob_thres(model_pred, labels):
     # threshold values
     xs = list(a / 10.0 for a in range(11))
 
     # all classes
-    total = [len(prob_with_threshold(df, i)) for i in xs]
+    total = [len(prob_with_threshold(model_pred, i)) for i in xs]
     line_total = plt.plot(xs, total, linewidth=2, label='total')
 
     for lb in labels:
-        lb_sum = [sum_category(prob_with_threshold(df, i),
-                                labels.index(lb)) for i in xs]
-        line_lb = plt.plot(xs, lb_sum, linewidth=2, label=lb)
+        # label index
+        lb_ind = labels.index(lb)
+        # size of the class
+        lb_size = [class_size(prob_with_threshold(model_pred,
+                                                  i))[lb_ind] for i in xs]
+        line_lb = plt.plot(xs, lb_size, linewidth=2, label=lb)
 
     plt.ylabel('Number of tracks')
     plt.xlabel('Probability')
     plt.legend()
-    sns.despine(top=True, right=False)
+    plt.show()
+
+def summary_of_prob_with_thres(model_pred, threshold, labels):
+    """Print summary with a threshold applied to
+    the model prediction.
+    """
+    model_thres = prob_with_threshold(model_pred, threshold)
+    print ("Total number of tracks to classify: {}."
+           "\nNumber of tracks with {} probability "
+           "of class membership: {}, "
+           "or {:.1%} of the initial assignment."
+           .format(len(model_pred),
+                   threshold,
+                   len(model_thres),
+                  (len(model_thres)/len(model_pred))))
+    print "\nAmong which..."
+    for lb in labels:
+        # label index
+        lb_ind = labels.index(lb)
+        # size of the class
+        lb_size = class_size(model_pred)[lb_ind]
+        # size of the class with a threshold
+        lb_thres_size = class_size(model_thres)[lb_ind]
+        print ("...assigned the \"{}\" class: {} "
+               "({:.1%} of the initial assignment.)"
+               "\nMean probability for the class: {:.3f}. \n"
+               .format(lb, lb_thres_size,
+                       (lb_thres_size / lb_size),
+                       class_mean(model_thres, lb_ind)))
+
